7924d6917377e64cda4c79c91eaa3b0d5acce5ae
kamilkorzeniewski <kamilkorzeniewski13@gmail.com>
2018-11-29 21:24:05 +0100

    Add Window.py with Window class Create empty window.

---
+++ Window.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import QWidget
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle("Some title")
+
+        self.show()
